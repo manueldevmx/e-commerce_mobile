faeb136e1888d726c18f853e9aa849a4ce8420b2
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-03-21 23:17:52 -0600

    RegisterForm

--- src/components/Auth/RegisterForm.jsx
+++ src/components/Auth/RegisterForm.jsx
@@ -1,24 +1,82 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 // create a component
 const RegisterForm = () => {
-    return (
-        <View style={styles.container}>
-            <Text>RegisterForm</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      {showLogin ? <Text>Form Login</Text> : <Text>Form Register</Text>}
+      <LottieView
+        source={require("../../../assets/shop.json")}
+        autoPlay
+        loop
+        style={{ margin: 33, marginBottom: 250 }}
+      />
+      <View>
+        <Text
+          style={{
+            marginTop: 120,
+            textAlign: "center",
+            fontSize: 29,
+            color: "#9119E7",
+            fontFamily: "Roboto",
+            fontWeight: "bold",
+          }}
+        >
+          Iniciar sesión
+        </Text>
+        <TextInput
+          mode="outlined"
+          style={{ marginTop: 20, width: 330, backgroundColor: "#FDD6BC" }}
+          label="Email"
+          value={text}
+          onChangeText={(text) => setText(text)}
+        />
+        <TextInput
+          mode="outlined"
+          style={{ marginTop: 20, width: 330, backgroundColor: "#FDD6BC" }}
+          label="Contraseña"
+          value={text}
+          onChangeText={(text) => setText(text)}
+        />
+      </View>
+      <Button
+        mode="contained"
+        style={{
+          marginTop: 90,
+          fontFamily: "Roboto",
+          borderBottomLeftRadius: 23,
+          borderTopRightRadius: 23,
+        }}
+        onPress={() => console.log("Pressed")}
+      >
+        Iniciar sesion
+      </Button>
+      <Text
+        style={{
+          marginTop: 9,
+          textAlign: "center",
+          fontSize: 14,
+          color: "#9119E7",
+          fontFamily: "Roboto",
+          fontWeight: "bold",
+        }}
+      >
+        ¿Olvidaste tu contraseña?
+      </Text>
+    </View>
+  );
 };
 
 // define your styles
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FDD6BC",
+  },
 });
 
 //make this component available to the app
--- src/components/screens/Auth.jsx
+++ src/components/screens/Auth.jsx
@@ -16,64 +16,7 @@ const Auth = () => {
   return (
     <View style={styles.container}>
     {showLogin ? <Text>Form Login</Text> : <Text>Form Register</Text>}
-      <LottieView
-        source={require("../../../assets/shop.json")}
-        autoPlay
-        loop
-        style={{ margin: 33, marginBottom: 250 }}
-      />
-      <View>
-        <Text
-          style={{
-            marginTop: 120,
-            textAlign: "center",
-            fontSize: 29,
-            color: "#9119E7",
-            fontFamily: "Roboto",
-            fontWeight: "bold",
-          }}
-        >
-          Iniciar sesión
-        </Text>
-        <TextInput
-          mode="outlined"
-          style={{ marginTop: 20, width: 330, backgroundColor: "#FDD6BC" }}
-          label="Email"
-          value={text}
-          onChangeText={(text) => setText(text)}
-        />
-        <TextInput
-          mode="outlined"
-          style={{ marginTop: 20, width: 330, backgroundColor: "#FDD6BC" }}
-          label="Contraseña"
-          value={text}
-          onChangeText={(text) => setText(text)}
-        />
-      </View>
-      <Button
-        mode="contained"
-        style={{
-          marginTop: 90,
-          fontFamily: "Roboto",
-          borderBottomLeftRadius: 23,
-          borderTopRightRadius: 23,
-        }}
-        onPress={() => console.log("Pressed")}
-      >
-        Iniciar sesion
-      </Button>
-      <Text
-        style={{
-          marginTop: 9,
-          textAlign: "center",
-          fontSize: 14,
-          color: "#9119E7",
-          fontFamily: "Roboto",
-          fontWeight: "bold",
-        }}
-      >
-        ¿Olvidaste tu contraseña?
-      </Text>
+     
     </View>
   );
 };
